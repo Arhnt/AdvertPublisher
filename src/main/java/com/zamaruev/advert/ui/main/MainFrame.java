cfe6a0d9f5d36169e2216682ddf3b4ea6d74f30d
Arhnt <vzamaruev@gmail.com>
2015-05-03 18:13:57 +0300

    Add sizes

--- src/main/java/com/zamaruev/advert/ui/main/MainFrame.java
+++ src/main/java/com/zamaruev/advert/ui/main/MainFrame.java
@@ -3,13 +3,9 @@ package com.zamaruev.advert.ui.main;
 import javax.swing.*;
 
 public class MainFrame extends JFrame {
+
     public MainFrame() {
-        setTitle("Advert Publisher");
+        setTitle("Advert Publisher Application");
         setSize(600, 400);
-        setLocationRelativeTo(null);
-        setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
-        JTable tbl = new JTable(new AdvertsTableModel());
-        JScrollPane sp = new JScrollPane(tbl);
-        this.getContentPane().add(sp, "Center");
     }
 }
